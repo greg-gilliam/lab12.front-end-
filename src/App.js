308b835f50e5aec60f80805c4318cd75912f00d8
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-24 09:43:55 -0700

    basic set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter, Switch } from 'react-router-dom';
+import signIn from './SignIn.js';
+import signUp from './SignUp.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// class App extends Component {
+//   state = {  }
+//   render() { 
+//     return ( 
+//         <BrowserRouter>
+//         <Switch>
+          
+//         </Switch>
+//         </BrowserRouter>
+//      );
+//   }
+// }
+ 
+console.log(signIn, signUp, )
+// export default App;
